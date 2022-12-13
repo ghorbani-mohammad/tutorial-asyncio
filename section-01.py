174957fc0b7119a6d3cc4caf73e6a24f96bc8245
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-13 21:07:17 +0330

    add output comment

--- section-01.py
+++ section-01.py
@@ -16,3 +16,12 @@ start_time = time.perf_counter()
 asyncio.run(main())
 elapsed = time.perf_counter() - start_time
 print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+
+# Output
+#   One
+#   One
+#   One
+#   Two
+#   Two
+#   Two
+#   countasync.py executed in 1.01 seconds.
